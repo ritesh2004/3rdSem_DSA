eb213572450218292b4b03d9302abb645671248d
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-02-04 17:53:55 +0530

    Modified Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,25 +2,37 @@
 using namespace std;
 
 void insertionSort(int arr[],int n){
-    int key,j;
-    for (int i = 1; i < n; i++)
+    int j;
+    for (int i = 0; i < n; i++)
     {
         /* code */
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j]>key )
+        j = i;
+        while (j > 0 && arr[j-1] > arr[j] )
         {
             /* code */
-            arr[j+1] = arr[j];
+            swap(arr[j-1],arr[j]);
             j = j - 1;
+            cout<<"Runs"<<endl;
         }
-        arr[j+1] = key;
     }
 }
 
+void swap(int a,int b){
+    int temp = b;
+    b = a;
+    a = temp;
+}
+
 int main(){
-    int arr[5] = {5,4,3,2,1};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[10];
+    int size;
+    cin>>size;
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+        cin>>arr[i];
+    }
+    
     insertionSort(arr,size);
     cout<<"---------- Sorted Array -----------"<<endl;
     for (int i = 0; i < size; i++)
